cc7e013f8fbc5f605f34219118e8522f3bb82e63
Albin COUSSON <albin.cousson@spurgin.fr>
2023-07-31 16:46:18 +0200

    refactor: ajout du nouveau CV

--- .next/static/webpack/webpack.59cb0409037cf8ac.hot-update.js
+++ .next/static/webpack/webpack.59cb0409037cf8ac.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a0d5933d5f11b4af"; }
+/******/ 	__webpack_require__.h = function() { return "91f75018569fdef8"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
